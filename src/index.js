8a82342ddc6ee51e5c24a9b56e9391ddedadec82
Rahul T George <34202955+rahul-george@users.noreply.github.com>
2025-11-19 17:01:18 +0100

    chore: removed unused code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import "./styles.css";
-// import { TaskCard } from "./components/task-card";
-
-// import { filterByToday, filterByProject } from "./filterMethods";
-
 import { Mediator } from "./services/mediator";
 
 import { ProjectModel } from "./models/project.model";
@@ -16,97 +12,8 @@ import { ProjectController } from "./controllers/project.controller";
 import { TodoController } from "./controllers/todo.controller";
 import { QuickFilterController } from "./controllers/quickfilter.controller";
 
-import {
-    initializeApp,
-    renderTasksByProject,
-    renderTasksByDate,
-} from "./commands";
-import { format } from "date-fns";
 import { FileStorage } from "./storage";
 import { FilterHelper } from "./services/filterhelper";
-
-// function editTask(event, task) {
-//   const taskIndex = tasks.findIndex((taskItem) => task.id == taskItem.id);
-//   tasks.splice(taskIndex, 1, task);
-//   console.log(tasks);
-//   renderTasks();
-// }
-
-// function deleteTask(event, task) {
-//   const taskIndex = tasks.findIndex((taskItem) => task.id == taskItem.id);
-//   tasks.splice(taskIndex, 1);
-//   renderTasks();
-// }
-
-// function renderTasks() {
-//   const taskList = document.querySelector("#tasks");
-//   Array.from(taskList.children).forEach((task) => task.remove());
-
-//   const filteredTasks = filters.reduce(
-//     (filteredTasks, fn) => fn(filteredTasks),
-//     tasks
-//   );
-
-//   filteredTasks.forEach((task) => {
-//     taskList.append(new TaskCard(task, editTask, deleteTask).render());
-//   });
-// }
-
-// const tasksTodayFilter = document.querySelector("#filter-tasks-today");
-// tasksTodayFilter.addEventListener("click", (e) => {
-//   filters.splice(0);
-//   filters.push(filterByToday);
-//   renderTasks();
-// });
-
-// const tasksInboxFilter = document.querySelector("#filter-tasks-inbox");
-// tasksInboxFilter.addEventListener("click", (e) => {
-//   filters.splice(0);
-//   filters.push(filterByProject("Inbox"));
-//   renderTasks();
-// });
-
-// renderTasks();
-//
-
-// const inboxElement = document.querySelector("#filter-tasks-inbox");
-// inboxElement.addEventListener("click", () => {
-//     renderTasksByProject("#tasks", "Inbox");
-// });
-
-// const todayElement = document.querySelector("#filter-tasks-today");
-// todayElement.addEventListener("click", (e) => {
-//     renderTasksByDate("#tasks", format(new Date(), "yyyy-MM-dd"));
-// });
-
-/*
-const createProjectButton = document.querySelector(".createProjectButton");
-const createProjectInput = document.querySelector("#projectCreateContainer");
-const closeProjectInput = document.querySelector("#closeProjectInput");
-const projectNameInput = document.querySelector("#projectNameInput");
-createProjectButton.addEventListener("click", (e) => {
-    createProjectInput.classList.remove("hidden");
-    createProjectButton.classList.add("hidden");
-});
-
-closeProjectInput.addEventListener("click", (e) => {
-    projectNameInput.value = "";
-    createProjectInput.classList.add("hidden");
-    createProjectButton.classList.remove("hidden");
-});
-
-projectNameInput.addEventListener("keydown", (e) => {
-    if (e.target.value === "") return;
-    if (e.key.toLowerCase() === "enter") {
-        console.log(e.target.value);
-        //fixme: Handle it
-        closeProjectInput.click();
-    }
-});
-
-initializeApp("#tasks");
-
-*/
 
 class App {
     constructor() {
